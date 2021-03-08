b9a62252ba19ef57a08ea71b093fba0d2af39cc6
jaclynnbarrera <jaclynnbarrera@gmail.com>
2021-03-08 15:03:15 -0500

    commit tree 653b7d3681e2f084edd73ea9e92d71b671b9f2df

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,34 +1,6 @@
 
 
-class ApplicationController < Sinatra::Base
-  register Sinatra::ActiveRecordExtension
-  set :session_secret, "my_application_secret"
-  set :views, Proc.new { File.join(root, "../views/") }
-
-
-  get '/' do
-    erb :index
-  end
-
-  get '/artists' do
-    @artists = Artist.all
-    erb:'artists/index'
-  end
-
-  get '/artists/:slug' do
-    @artist = Artist.find_by_slug(params[:slug])
-    erb :'artists/show'
-  end
-
-  get '/genres' do
-    @genres = Genre.all
-    erb :'genres/index'
-  end
-
-  get '/genres/:slug' do
-    @genre = Genre.find_by_slug(params[:slug])
-    erb :'/genres/show'
-  end
+class SongsController < ApplicationController
 
   get '/songs' do
     @songs = Song.all
@@ -70,4 +42,5 @@ class ApplicationController < Sinatra::Base
     flash[:message] = "Successfully updated song."
     redirect("/songs/#{@song.slug}")
   end
+
 end
